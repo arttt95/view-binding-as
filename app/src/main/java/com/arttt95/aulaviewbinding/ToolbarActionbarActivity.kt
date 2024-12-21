16e1c94dde11f5541b2678cca0bca968e93dd8c6
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-12-21 14:29:33 -0300

    21DEZ24 - Aula 167 AS - Toolbar & Actionbar.

---
+++ app/src/main/java/com/arttt95/aulaviewbinding/ToolbarActionbarActivity.kt
@@ -0,0 +1,32 @@
+package com.arttt95.aulaviewbinding
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.arttt95.aulaviewbinding.databinding.ActivityToolbarActionbarBinding
+
+class ToolbarActionbarActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityToolbarActionbarBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        supportActionBar?.hide()
+
+        with(binding) {
+
+        }
+
+    }
+}
